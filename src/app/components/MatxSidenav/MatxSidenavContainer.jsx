78a7f6c55ac415af1b4028a0753ad3622883684b
johnatan-si <johnatan-si@hotmail.com>
2022-01-24 16:34:03 -0300

    Delete src directory

--- src/app/components/MatxSidenav/MatxSidenavContainer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const MatxSidenavContainer = ({ children }) => {
-    return <div className="relative flex h-full">{children}</div>
-}
-
-export default MatxSidenavContainer
--- src/app/components/MatxSidenav/MatxSidenavContent.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const MatxSidenavContent = ({ children }) => {
-    return <div className=" h-full">{children}</div>
-}
-
-export default MatxSidenavContent
