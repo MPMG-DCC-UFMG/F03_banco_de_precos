4b1cc0f5ee83f9c87b91713a55ed7c6da89fd7e5
Rodrigo Denúbila <rdenubila@gmail.com>
2022-05-17 22:42:27 -0300

    localization to ptBr

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,27 @@ import GlobalContext from './wrappers/GlobalContext';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Result from './pages/Results';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { ptBR } from '@mui/x-data-grid';
+import { ptBR as corePtBr } from '@mui/material/locale';
+
+const theme = createTheme(
+  corePtBr,
+  ptBR
+);
 
 function App() {
   return (
-    <GlobalContext>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/result" element={<Result />} />
-        </Routes>
-      </BrowserRouter>
-    </GlobalContext>
+    <ThemeProvider theme={theme}>
+      <GlobalContext>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/result" element={<Result />} />
+          </Routes>
+        </BrowserRouter>
+      </GlobalContext>
+    </ThemeProvider>
   );
 }
 
